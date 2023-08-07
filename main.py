f9befe69e8e2e3c3e28e17bbcb40e737f0783923
stupidaaryan <141643484+stupidaaryan@users.noreply.github.com>
2023-08-07 19:10:33 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+que = input("do u wanna no something? Y for yes and N for no \n").lower()
+
+if que == "y" :
+  print("i know it is a dumb project but this is my first project in python and i will try to learn python 1 hour daily for 30 days then i will see if i wanna continue or not")
+else:
+  print("Fuck you")
